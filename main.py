87dcc4d6ec40a541d5449e72802731b62e1d58f6
mertosma <72814726+mertosma@users.noreply.github.com>
2021-10-08 14:11:06 +0000

    main sh

--- main.py
+++ main.py
@@ -1,5 +1,3 @@
 #!/bin/sh
-mkdir profiles2021
-touch profiles2021/helin.txt
-ls
+
 
--- main.sh
+++ main.sh
@@ -11,10 +11,17 @@ rm ids.txt
 
 
 # Command for Q1
-
+touch quiz.sh
 # Command for Q2
-
+chmod u+rwx quiz.sh
+./quiz.sh
 # Command for Q3
-
+mkdir profiles2021
+touch mert.txt
+cp mert.txt ./profiles2021
+echo Name:Mert Osma > ./profiles2021/mert.txt
+echo KUID:72018 >> ./profiles2021/mert.txt
+echo email:mosma19@ku.edu.tr >> ./profiles2021/mert.txt
+cat ./profiles2021/mert.txt
 # Command for Q4
-
+cp ./profiles2020/alara.txt ./profiles2021 | cp ./profiles2020/burak.txt ./profiles2021 | cp ./profiles2020/can.txt ./profiles2021 | cp ./profiles2020/dilan.txt ./profiles2021
